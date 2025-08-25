c137c1871cb75d3035a67fb25bc0e67acaa083d5
zeref <abhijeetsingh173+vscode@gmail.com>
2025-08-25 14:25:14 +0530

    email

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -91,13 +91,13 @@ export default function Login() {
           {/* Email Input */}
           <div className="mb-4">
             <label className="block text-sm font-medium text-gray-700 mb-1">
-              Email Address
+             College Email
             </label>
             <input
               onChange={handleChange}
               name="email"
               type="email"
-              placeholder="example@domain.com"
+              placeholder="ID@nitjsr.ac.in"
               className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600 transition placeholder-gray-400"
               value={loginInfo.email}
             />
